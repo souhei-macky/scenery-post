324cea139c39417a0199c04e38c2c5ccad8346aa
souhei-macky <souhei515151@gmail.com>
2019-11-21 14:45:51 +0900

    index.view.

---
+++ db/migrate/20191121051420_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :genre
+      t.string :title
+      t.string :text
+      t.text :image
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20191121051420) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.string   "title"
+    t.string   "text"
+    t.text     "image",      limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
 end
